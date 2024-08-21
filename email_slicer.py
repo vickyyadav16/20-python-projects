18017abd4673203baabb326e22233cbf8ed0dffc
vickyyadav16 <116965319+vickyyadav16@users.noreply.github.com>
2024-08-21 18:17:41 +0530

    use for email slicing

---
+++ email_slicer.py
@@ -0,0 +1,21 @@
+# collect email from user
+# split the email using the @, the first part as the user name, the second part is gonna be saved as domain
+# split domain using ..
+
+def main():
+    print(" Welcome to the email slicer")
+    print("")
+
+    email_input = input("Input your email address:")
+
+    (username,domain) = email_input.split("@")
+    (domain, extension) = domain.split(".")
+
+    print("Username : ", username)
+    print("Domain :", domain)
+    print("Extension:", extension)
+
+while True:
+    main()
+    
+
